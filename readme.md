900183de0fcd8d0f82c812f4fa92bcd206c967f6
Jack Moore <hello@jacklmoore.com>
2015-05-20 04:06:05 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+## Summary
+
+A `position: sticky` polyfill that's dependency-free and does not modify the DOM tree, i.e., React-friendly.
+
+#### Demo
+
+Todo
+
+##### Install via NPM
+```bash
+npm install sticky-position
+```
+##### Install via Bower
+```bash
+bower install sticky-position
+```
+
+Released under the [MIT License](http://www.opensource.org/licenses/mit-license.php)
---
+++ src/sticky-position.js
@@ -0,0 +1,103 @@
+export default function({
+	primary = null,
+	placeholder = null,
+	wrapper = null,
+	computeWidth = true,
+} = {}){
+	let top = null;
+	let isSticky = false;
+
+	const nativeSupport = (function(){		
+		if (this.isSupported !== null) {
+			return this.isSupported;
+		} else {
+			const style = document.createElement('test').style;
+			style.cssText = ['-webkit-', '-ms-', ''].map(prefix => `position: ${prefix}sticky`).join(';');
+			this.isSupported = style.position.indexOf('sticky') !== -1;
+			return this.isSupported;
+		}
+	}).bind({isSupported: null});
+
+	function stick() {
+		if (isSticky === true) return;
+		primary.style.position = 'fixed';
+		isSticky = true;
+	}
+
+	function unstick() {
+		if (isSticky === false) return;
+		primary.style.position = 'relative';
+		primary.style.width = '';
+		primary.style.top = '';
+		primary.style.left = '';
+		placeholder.style.height = '';
+		placeholder.style.width = '';
+		isSticky = false;
+	}
+
+	function init() {
+		// positioning necessary for getComputedStyle to report the correct z-index value.
+		wrapper.style.position = 'relative';
+
+		const style = window.getComputedStyle(wrapper, null);
+
+		top = parseInt(style.top) || 0;
+		primary.style.zIndex = style.zIndex;
+		primary.style.position = 'relative';
+		wrapper.style.top = 0;
+		// Giving the placeholder an overflow of 'hidden' or 'auto' will allow it to clear any bottom margin extending beneath the primary element.
+		// Clearing that margin is needed so that it's contribution to the wrapper element's height can be measured with getBoundingClientRect.
+		placeholder.style.overflow = 'hidden';
+
+		update();
+		window.addEventListener('load', update);
+		window.addEventListener('scroll', update);
+		window.addEventListener('resize', update);
+	}
+
+	function update() {
+		const rect = wrapper.getBoundingClientRect();
+		const sticky = rect.top < top;
+		
+		if (sticky) {
+			placeholder.style.height = rect.height+'px';
+			if (computeWidth) {
+				placeholder.style.width = rect.width+'px';
+			}
+
+			const parentRect = wrapper.parentNode.getBoundingClientRect();
+
+			primary.style.top  = Math.min(parentRect.top + parentRect.height - rect.height, top)+'px';
+
+			if (computeWidth) {
+				primary.style.width = rect.width+'px';
+			}
+			primary.style.left = rect.left+'px';
+
+			stick();
+		} else {
+			unstick();
+		}	
+	}
+
+	function destroy() {
+		window.removeEventListener('load', update);
+		window.removeEventListener('scroll', update);
+		window.removeEventListener('resize', update);
+		unstick();
+	}
+	
+	if (nativeSupport()) {
+		return {
+			update(){},
+			destroy(){}
+		};
+	} else {
+		init();
+
+		return {
+			update,
+			destroy,
+		};
+	}
+}
