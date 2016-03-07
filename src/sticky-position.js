d387e677e6e22526bcc8f12cadedce41191ab18f
JP McGarrity <john_mcgarrity@condenast.com>
2016-03-07 15:56:23 -0500

    Added isSupported arg

--- src/sticky-position.js
+++ src/sticky-position.js
@@ -1,101 +1,108 @@
-export default function({
-	primary = null,
-	placeholder = null,
-	wrapper = null,
-	computeWidth = true,
-} = {}){
-	let top = null;
-	let isSticky = false;
+'use strict';
 
-	const nativeSupport = (function(){		
-		if (this.isSupported !== null) {
-			return this.isSupported;
-		} else {
-			const style = document.createElement('test').style;
-			style.cssText = ['-webkit-', '-ms-', ''].map(prefix => `position: ${prefix}sticky`).join(';');
-			this.isSupported = style.position.indexOf('sticky') !== -1;
-			return this.isSupported;
-		}
-	}).bind({isSupported: null});
+export default ({
+  primary = null,
+  placeholder = null,
+  wrapper = null,
+  computeWidth = true,
+  isSupported = null
+} = {}) => {
+  let top = null;
+  let isSticky = false;
+  isSupported = isSupported ? null : isSupported;
 
-	function stick() {
-		if (isSticky === true) return;
-		primary.style.position = 'fixed';
-		isSticky = true;
-	}
+/*
+ * @description If default.isSupported is anything except `null` the polyfill will use the JS method.
+ */
+  const nativeSupport = (function(){
+    if (this.isSupported !== null) {
+      return this.isSupported;
+    } else {
+      const style = document.createElement('test').style;
+      style.cssText = ['-webkit-', '-ms-', ''].map(prefix => `position: ${prefix}sticky`).join(';');
+      this.isSupported = style.position.indexOf('sticky') !== -1;
+      return this.isSupported;
+    }
+  }).bind({isSupported: isSupported});
 
-	function unstick() {
-		if (isSticky === false) return;
-		primary.style.position = 'relative';
-		primary.style.width = '';
-		primary.style.top = '';
-		primary.style.left = '';
-		placeholder.style.height = '';
-		placeholder.style.width = '';
-		isSticky = false;
-	}
+  function stick() {
+    if (isSticky === true) return;
+    primary.style.position = 'fixed';
+    isSticky = true;
+  }
 
-	function init() {
-		// positioning necessary for getComputedStyle to report the correct z-index value.
-		wrapper.style.position = 'relative';
+  function unstick() {
+    if (isSticky === false) return;
+    primary.style.position = 'relative';
+    primary.style.width = '';
+    primary.style.top = '';
+    primary.style.left = '';
+    placeholder.style.height = '';
+    placeholder.style.width = '';
+    isSticky = false;
+  }
 
-		const style = window.getComputedStyle(wrapper, null);
+  function init() {
+    // positioning necessary for getComputedStyle to report the correct z-index value.
+    wrapper.style.position = 'relative';
 
-		top = parseInt(style.top) || 0;
-		primary.style.zIndex = style.zIndex;
-		primary.style.position = 'relative';
-		wrapper.style.top = 0;
-		// Giving the placeholder an overflow of 'hidden' or 'auto' will allow it to clear any bottom margin extending beneath the primary element.
-		// Clearing that margin is needed so that it's contribution to the wrapper element's height can be measured with getBoundingClientRect.
-		placeholder.style.overflow = 'hidden';
+    const style = window.getComputedStyle(wrapper, null);
 
-		update();
-		window.addEventListener('load', update);
-		window.addEventListener('scroll', update);
-		window.addEventListener('resize', update);
-	}
+    top = parseInt(style.top) || 0;
+    primary.style.zIndex = style.zIndex;
+    primary.style.position = 'relative';
+    wrapper.style.top = 0;
+    // Giving the placeholder an overflow of 'hidden' or 'auto' will allow it to clear any bottom margin extending beneath the primary element.
+    // Clearing that margin is needed so that it's contribution to the wrapper element's height can be measured with getBoundingClientRect.
+    placeholder.style.overflow = 'hidden';
 
-	function update() {
-		const rect = wrapper.getBoundingClientRect();
-		const sticky = rect.top < top;
-		
-		if (sticky) {
-			placeholder.style.height = rect.height + 'px';
-			
-			if (computeWidth) {
-				placeholder.style.width = rect.width + 'px';
-			}
-			
-			var parentRect = wrapper.parentNode.getBoundingClientRect();
-			
-			primary.style.top = Math.min(parentRect.top + parentRect.height - rect.height, top) + 'px';
-			primary.style.width = computeWidth ? rect.width+'px' : '100%';
-			primary.style.left = rect.left + 'px';
-			
-			stick();
-		} else {
-			unstick();
-		}	
-	}
+    update();
+    window.addEventListener('load', update);
+    window.addEventListener('scroll', update);
+    window.addEventListener('resize', update);
+  }
 
-	function destroy() {
-		window.removeEventListener('load', update);
-		window.removeEventListener('scroll', update);
-		window.removeEventListener('resize', update);
-		unstick();
-	}
-	
-	if (nativeSupport()) {
-		return {
-			update(){},
-			destroy(){}
-		};
-	} else {
-		init();
+  function update() {
+    const rect = wrapper.getBoundingClientRect();
+    const sticky = rect.top < top;
 
-		return {
-			update,
-			destroy,
-		};
-	}
+    if (sticky) {
+      placeholder.style.height = rect.height + 'px';
+
+      if (computeWidth) {
+        placeholder.style.width = rect.width + 'px';
+      }
+
+      var parentRect = wrapper.parentNode.getBoundingClientRect();
+
+      primary.style.top = Math.min(parentRect.top + parentRect.height - rect.height, top) + 'px';
+      primary.style.width = computeWidth ? rect.width+'px' : '100%';
+      primary.style.left = rect.left + 'px';
+
+      stick();
+    } else {
+      unstick();
+    }
+  }
+
+  function destroy() {
+    window.removeEventListener('load', update);
+    window.removeEventListener('scroll', update);
+    window.removeEventListener('resize', update);
+    unstick();
+  }
+
+  if (nativeSupport()) {
+    return {
+      update(){},
+      destroy(){}
+    };
+  } else {
+    init();
+
+    return {
+      update,
+      destroy,
+    };
+  }
 }
